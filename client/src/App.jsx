b4a5ba7b272620906a74ed4f590bde63918b443b
Kym Hooper <khoupp@gmail.com>
2020-11-23 10:03:28 -0700

    Query all to-dos, initialize full list with componentDidMount

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
-// import query from './routes';
+import query from '../lib/routes'
 import Container from 'react-bootstrap/Container';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      todoList: [],
+      todoItem: '',
+    };
   }
-  // componentDidMount() {};
+  componentDidMount() {
+    this.getTodos();
+  };
 
-
-
+  getTodos() {
+    query.getAllTodos((err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(data.data.rows);
+        this.setState({todoList: data.data.rows});
+      }
+    });
+  }
 
   render() {
-    const {} = this.state;
+    const {todoList,} = this.state;
     return (
       <div>
         <Container className="to-do-container">
