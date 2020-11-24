73c13be4bcc15c715bcf3cb671114980bf5664f2
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:55:33 -0700

    Update routes.js axios functions to import to App / client side

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,27 +1,30 @@
+import React from 'react';
 import axios from 'axios';
 
 const getAllTodos = (cb) => {
-  axios.get(`http://localhost:3000/todos/`)
+  return axios.get(`http://localhost:3000/todos/`)
       .then((data) => cb(null, data))
       .catch((err) => cb(err, null));
 };
 
-const postTodo = (newTodo, cb) => {
-  axios.post(`http://localhost:3000/todos/`, newTodo)
-    .then(() => cb(null))
-    .catch((err) => cb(err, null));
-}
+const postTodo = (newTodo) => {
+  console.log("new todo: ", newTodo);
+  return axios.post(`http://localhost:3000/todos`, newTodo)
+    .then((res) => res.data)
+    .catch((err) => console.log('Error posting from client: ', err));
+};
 
-const putTodo = (id, cb) => {
-  axios.put(`http://localhost:3000/todos/${id}`)
-    .then(() => cb(null))
-    .catch((err) => cb(err, null));
-}
+const putTodo = (id) => {
+  return axios.put(`http://localhost:3000/todos/${id}`)
+    .then((res) => res.data)
+    .catch((err) => console.log('Error completing task: ', err));
+};
 
-const deleteTodo = (id, cb) => {
-  axios.delete(`http://localhost:3000/todos/${id}`)
-    .then((data) => cb(null, data))
-    .catch((err) => cb(err, null));
+const deleteTodo = (id) => {
+  console.log('ID: ', id);
+  return axios.delete(`http://localhost:3000/todos/${id}`)
+    .then((res) => res.data)
+    .catch((err) => console.log('Error deleting from client: ', err));
 }
 
 export default {
