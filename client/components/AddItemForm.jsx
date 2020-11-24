2013011ebc131f7a8bad0bb817a78b930b1aed09
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:53:22 -0700

    Form to input a todo, submitting this adds an item to the list

---
+++ client/components/AddItemForm.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+
+const AddItemForm = ({onHide, getTodos, handleInput, handleSubmit}) => {
+
+  return (
+    <div>
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control size="lg" type="text" placeholder="To-do Item"
+            onChange={handleInput}/>
+          </Col>
+          <Button variant="outline-secondary"
+          type="submit"
+          onClick={handleSubmit}
+          // onClick={(e) => { onHide; handleSubmit}}
+          >
+            Submit
+          </Button>
+        </Form.Row>
+      </Form>
+    </div>
+  );
+};
+
+export default AddItemForm;
