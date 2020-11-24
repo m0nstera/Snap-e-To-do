291c34c2e133118e4f78cd0a85bdf8c250360c70
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:56:41 -0700

    Add console logs to test where the requests are hitting

--- server/server.js
+++ server/server.js
@@ -25,11 +25,14 @@ app.get('/todos', (req, res) => {
 // INSERT -- one todo
 app.post("/todos", (req, res) => {
   let body = req.body;
+  console.log("body: ", body);
   insertNewTodo(body, (err, results) => {
     if (err) {
-      res.sendStatus(404);
+      console.log('Post query unsuccessful');
+      res.status(400);
     } else {
-      res.sendStatus(201);
+      console.log('Post query successful');
+      res.status(201).send(results);
     }
   })
 });
@@ -39,22 +42,23 @@ app.put("/todos/:id", (req, res) => {
     let id = req.params.id;
     updateTodo(id, (err, results) => {
       if (err) {
-        res.sendStatus(404);
+        console.log('Completion error from DB')
+        res.status(404);
       } else {
-        res.sendStatus(201);
+        console.log('Completion successful from database');
+        res.status(201).send(results);
       }
     })
   });
-
 
 // DELETE -- one todo
 app.delete("/todos/:id", (req, res) => {
   let id = req.params.id;
   removeTodo(id, (err, results) => {
     if (err) {
-      res.sendStatus(404);
+      res.status(404);
     } else {
-      res.send(results);
+      res.status(200).send(results);
     }
   })
 });
