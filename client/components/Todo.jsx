669b3206b9c99dbfe85a420a5eaebad694f1e7b5
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:48:13 -0700

    Add checkbox, link id to put request

--- client/components/Todo.jsx
+++ client/components/Todo.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import DeleteBtn from './DeleteBtn.jsx';
+import query from '../lib/routes';
 
-const Todo = ({todo}) => {
+const Todo = ({todo, handleDelete, handleComplete}) => {
 
   return (
     <div>
       <ul>
-        <li>
-          {todo.item}
-        </li>
+        <Row>
+          <Col>
+            <li>
+              <input
+              type="checkbox"
+              id={todo.id}
+              name={todo.item}
+              value={todo.id}
+              onClick={handleComplete}/>
+              <label for={todo.item}> &nbsp; {todo.item}</label>
+            </li>
+          </Col>
+          <Col>
+            <DeleteBtn
+            handleDelete={handleDelete}
+            id={todo.id}/>
+          </Col>
+        </Row>
       </ul>
     </div>
   );
