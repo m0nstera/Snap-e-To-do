3d019a81bf4b31851899ff38d1e6c23a2cc6e365
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:54:23 -0700

    Update queries syntax err, missing quotes around body.item

--- database/queries.js
+++ database/queries.js
@@ -1,6 +1,7 @@
 const { connection } = require('./connection.js');
 connection.connect();
 
+//order by completed or created at time, ascending v descending
 const selectAllTodos = (cb) => {
   connection.query(`SELECT * FROM public."snapTodos"`, (err, results) => {
     if (err) {
@@ -12,18 +13,19 @@ const selectAllTodos = (cb) => {
 };
 
 const insertNewTodo = (body, cb) => {
-  // console.log('BODY ', body);
-  connection.query(`INSERT into public."snapTodos" (item) values(${body.item})`, (err, results) => {
+  connection.query(`INSERT INTO public."snapTodos"(item) VALUES ('${body.item}');`, (err, results) => {
     if (err) {
+      console.log('Error posting to DB');
       cb(err, null);
     } else {
+      console.log('Success posting todo to DB');
       cb(null, results);
     }
   });
 };
 
 const updateTodo = (id, cb) => {
-  connection.query(`Update public."snapTodos" SET completed = NOT completed WHERE id = ${id}`, (err, results) => {
+  connection.query(`UPDATE public."snapTodos" SET completed = NOT completed WHERE id = ${id}`, (err, results) => {
     if (err) {
       cb(err, null);
     } else {
@@ -32,12 +34,13 @@ const updateTodo = (id, cb) => {
   });
 };
 
-const removeTodo = (todoObj, cb) => {
-  let todoItem = todoObj.item;
-  connection.query(`DELETE FROM public."snapTodos" WHERE item = ${todoItem}`, (err, results) => {
+const removeTodo = (id, cb) => {
+  connection.query(`DELETE FROM public."snapTodos" WHERE id = ${id}`, (err, results) => {
     if (err) {
+      console.log('Error deleting from Db');
       cb(err, null);
     } else {
+      console.log('Entry deleted from DB');
       cb(null, results);
     }
   });
