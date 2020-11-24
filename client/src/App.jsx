120ddaf9891925284f6dad5d6210d3c2a45e0762
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:50:04 -0700

    Build event handlers and query the database from the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import query from '../lib/routes';
 import TodoList from '../components/TodoList.jsx';
 import Container from 'react-bootstrap/Container';
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       todoList: [],
-      todoItem: '',
+      completed: [],
+      item: '',
     };
+    this.getTodos = this.getTodos.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleComplete = this.handleComplete.bind(this);
   }
+
   componentDidMount() {
     this.getTodos();
   };
@@ -21,22 +28,57 @@ class App extends React.Component {
       if (err) {
         throw err;
       } else {
-        console.log(data.data.rows);
         this.setState({todoList: data.data.rows});
       }
     });
-  }
+  };
+
+  handleInput(e) {
+    this.setState({item: e.target.value});
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const item = this.state.item;
+    console.log("item: ", item);
+    query.postTodo({item: item})
+      .then(res => res)
+      .then(this.getTodos())
+      .catch(err => console.log('Error: ', err));
+  };
+
+  handleDelete(e) {
+    e.preventDefault();
+    const id = e.target.id;
+    console.log('APP ID: ', id);
+    query.deleteTodo(id)
+      .then(res => res)
+      .then(this.getTodos())
+      .catch(err => console.log('Error deleting: ', err));
+  };
+
+  handleComplete(e) {
+    e.preventDefault();
+    const id = e.target.id;
+    console.log('APP ID', id);
+    query.putTodo(id)
+      .then(res => res)
+      .then(this.getTodos())
+      .catch(err => console.log('Error completing: ', err));
+  };
 
   render() {
     const {todoList,} = this.state;
     return (
       <div>
         <Container className="to-do-container">
-          <div>
-            <h1>To-do Items</h1>
           <TodoList
-            todoList={todoList}/>
-          </div>
+            todoList={todoList}
+            getTodos={this.getTodos}
+            handleInput={this.handleInput}
+            handleSubmit={this.handleSubmit}
+            handleDelete={this.handleDelete}
+            handleComplete={this.handleComplete}/>
         </Container>
       </div>
     );
