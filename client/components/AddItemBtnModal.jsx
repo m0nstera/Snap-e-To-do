22f47eb32f172cf074d90b9f62d958b6420aba50
Kym Hooper <khoupp@gmail.com>
2020-11-24 15:52:22 -0700

    Build a button that opens a modal/form to add an item to the list

---
+++ client/components/AddItemBtnModal.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import AddItemForm from './AddItemForm.jsx';
+
+const AddItemBtnModal = ({getTodos, handleInput, handleSubmit}) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button
+        className="add-to-do"
+        variant="dark"
+        size="md"
+        onClick={handleShow}
+      >Add Item
+      </Button>
+
+      <Modal
+        centered
+        show={show}
+        onHide={handleClose}
+      >
+        <Modal.Header closeButton>
+          Add an item to your list!
+        </Modal.Header>
+        <Modal.Body>
+          <AddItemForm
+            onHide={handleClose}
+            getTodos={getTodos}
+            handleInput={handleInput}
+            handleSubmit={handleSubmit}
+          />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddItemBtnModal;
