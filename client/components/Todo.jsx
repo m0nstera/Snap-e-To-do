201616678bccf6c2b3225bef1ff237caa2ef72bf
Kym Hooper <khoupp@gmail.com>
2020-11-29 18:23:28 -0700

    Edit Todo component

--- client/components/Todo.jsx
+++ client/components/Todo.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import DeleteBtn from './DeleteBtn.jsx';
+import React, {useState}  from 'react';
 import query from '../lib/routes';
+import DeleteBtn from './DeleteBtn.jsx';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 
-const Todo = ({todo, handleDelete, handleComplete}) => {
+const Todo = ({todo, handleDelete, handleComplete, handleChange, handleInput, item, checked, handleCheck}) => {
+  // const [clicked, setClick] = useState([]);
+  // const handleCheck = (e) => {
+  //   setClick({ ...clicked, [e.target.name]: e.target.checked });
+  // };
+  // const handleCheck = ({ target : { item, checked } }) => {
+  //   setClick({
+  //     ...clicked,
+  //     [item]: checked
+  //   })
+  // }
+  // const handleShow = (item) => {
+  //   setClick(item);
+  // };
 
   return (
     <div>
-      <ul>
         <Row>
           <Col>
-            <li>
-              <input
+            <ListGroup.Item>
+            <Form.Check
               type="checkbox"
               id={todo.id}
+              label={todo.item}
               name={todo.item}
               value={todo.id}
-              onClick={handleComplete}/>
-              <label for={todo.item}> &nbsp; {todo.item}</label>
-            </li>
+              onClick={handleComplete}
+              onChange={handleCheck}
+              checked={checked}
+              // onChange={handleInput}
+              // onChange={e => onChange(e.target.checked)}
+              // checked={clicked[todo.item]}
+              custom
+            />
+            </ListGroup.Item>
           </Col>
           <Col>
             <DeleteBtn
@@ -27,9 +49,18 @@ const Todo = ({todo, handleDelete, handleComplete}) => {
             id={todo.id}/>
           </Col>
         </Row>
-      </ul>
     </div>
   );
 };
 
 export default Todo;
+
+              /* <input
+              type="checkbox"
+              checked={checked}
+              id={todo.id}
+              name={todo.item}
+              value={todo.id}
+              onClick={handleComplete}
+              />
+              <label for={todo.item}> &nbsp; {todo.item}</label> */
