6951857471b9dac9deaeeb98928f68eb9955d782
Kym Hooper <khoupp@gmail.com>
2020-11-30 19:17:24 -0700

    Clean up props drilling, organize Row and Col for styling

--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -1,33 +1,39 @@
 import React, {useState} from 'react';
+import Todo from './Todo.jsx';
+import AddItemBtnModal from './AddItemBtnModal.jsx';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Todo from './Todo.jsx';
-import AddItemBtnModal from './AddItemBtnModal.jsx';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const TodoList = ({todoList, getTodos, handleInput, handleSubmit, handleDelete, handleComplete}) => {
   const mappedList = todoList.map((todo, i) => <Todo
     todo={todo}
-    key={i}
+    key={todo.id}
     handleDelete={handleDelete}
     handleComplete={handleComplete}
     />);
 
   return (
-    <Container>
-      <Row>
+    <div>
+      <Row
+        className="h1-add-item-row"
+        xs={12} md={12} lg={12}>
         <Col>
           <h1>To-do Items</h1>
         </Col>
         <Col>
           <AddItemBtnModal
+            className="add-item-btn"
             getTodos={getTodos}
             handleInput={handleInput}
             handleSubmit={handleSubmit}/>
         </Col>
       </Row>
-      {mappedList}
-    </Container>
+      <ListGroup>
+        {mappedList}
+      </ListGroup>
+    </div>
   )
 };
 
