e114aa95c2237c45a1559b7c3af982ee13244a8c
Kym Hooper <khoupp@gmail.com>
2020-11-30 21:29:02 -0700

    Install and import react-minimal-pie-chart; render complete/incomplete tasks

---
+++ client/components/PieChartComp.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import { PieChart } from 'react-minimal-pie-chart';
+
+const PieChartComp = ({todoList}) => {
+  // console.log("PIE", todoList);
+  // const filterTrue = todoList.map((todo, i) => todo.completed === true);
+  // const filterFalse = todoList.map((todo, i) => todo.completed === false);
+  const mappedPie = todoList.map((todo, i) =>
+  todo.completed === true
+  ? {title: 'Complete', value: todo.completed === true, color: '#E38627'}
+  : {title: 'Incomplete', value: todo.completed === false, color: '#6A2135'});
+
+  const defaultLabelStyle = {
+    fontSize: '10px',
+    fontFamily: 'sans-serif',
+  };
+
+  return (
+    <div>
+      <Row>
+        <h4 className="pie-heading">
+          Complete vs. incomplete tasks
+        </h4>
+      </Row>
+      <Row className="pie-chart">
+        <PieChart
+        data={mappedPie}
+        lineWidth={65}
+        paddingAngle={5}
+        label={(data) => data.dataEntry.title}
+        labelPosition={65}
+        labelStyle={{
+          ...defaultLabelStyle,
+        }}
+        style={{ height: '130px' }}
+        />
+      </Row>
+    </div>
+
+  )
+};
+
+export default PieChartComp;
