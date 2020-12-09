8e1a97b02978793aae64d0b63ff2fc33707bc57e
Kym Hooper <khoupp@gmail.com>
2020-12-09 16:30:21 -0700

    Prep to use hooks or instantiate a new class

--- client/components/LineChartComp.jsx
+++ client/components/LineChartComp.jsx
@@ -1,59 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Chart from 'react-apexcharts';
 
-// const LineChartComp = () => {};
+const LineChartComp = () => {
+  const [completed, setCompleted] = useState(0);
 
-
-class LineChartComp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-      series: [{
-          name: "Desktops",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-      }],
-      options: {
-        chart: {
-          height: 350,
-          type: 'line',
-          zoom: {
-            enabled: true
-          }
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'straight'
-        },
-        title: {
-          text: 'Visualize Productivity',
-          align: 'left'
-        },
-        grid: {
-          row: {
-            colors: ['#AAAAAA', 'transparent'],
-            opacity: 0.5
-          },
-        },
-        xaxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-        }
-      },
-
-
-    };
-  }
-
-  render() {
-    return (
+  return (
       <div id="chart">
-        <LineChartComp options={this.state.options} series={this.state.series} type="line" height={350} />
+        <LineChartComp
+          options={this.state.options}
+          series={this.state.series}
+          type="line"
+          height={350} />
       </div>
-    );
-  }
-}
+  );
+};
+
+
+// class LineChartComp extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+
+//       series: [{
+//           name: "Desktops",
+//           data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+//       }],
+//       options: {
+//         chart: {
+//           height: 350,
+//           type: 'line',
+//           zoom: {
+//             enabled: true
+//           }
+//         },
+//         dataLabels: {
+//           enabled: false
+//         },
+//         stroke: {
+//           curve: 'straight'
+//         },
+//         title: {
+//           text: 'Visualize Productivity',
+//           align: 'left'
+//         },
+//         grid: {
+//           row: {
+//             colors: ['#AAAAAA', 'transparent'],
+//             opacity: 0.5
+//           },
+//         },
+//         xaxis: {
+//           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+//         }
+//       },
+
+
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div id="chart">
+//         <LineChartComp options={this.state.options} series={this.state.series} type="line" height={350} />
+//       </div>
+//     );
+//   }
+// }
 
 // export default LineChartComp;
